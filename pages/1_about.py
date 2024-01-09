74689da30c81123c5ab5e179f2c313a5786bf026
Han <phamhan@uw.edu>
2024-01-08 21:47:39 -0800

    remove streamlit_extras components

--- pages/1_about.py
+++ pages/1_about.py
@@ -1,6 +1,6 @@
 import streamlit as st
-from streamlit_extras.buy_me_a_coffee import button
-from streamlit_extras.let_it_rain import rain
+# from streamlit_extras.buy_me_a_coffee import button
+# from streamlit_extras.let_it_rain import rain
 
 st.set_page_config(page_title="About", page_icon="🦦")
 
@@ -10,6 +10,6 @@ st.sidebar.markdown("# About ")
 st.write("CS Undergrad @UWB")
 st.write("See more at: [hanspham.com](https://hanspham.com)")
 
-button(username="hanspham", floating=False, width=221)
+# button(username="hanspham", floating=False, width=221)
 
-rain(emoji="🌈", font_size=30, falling_speed=10, animation_length=5)
+# rain(emoji="🌈", font_size=30, falling_speed=10, animation_length=5)
